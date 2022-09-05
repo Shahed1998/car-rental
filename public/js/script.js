e03af6ff07a8108b8de236e967155707861f1433
Shahed1998 <shahedchowdhury120@gmail.com>
2022-09-05 14:51:17 +0600

    ...

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 // Navbar section
 const setActive = (anc) => {
   const navLinks = document.querySelectorAll('.nav-link');
+
   navLinks.forEach((link) => {
     link.classList.remove('active');
   });
